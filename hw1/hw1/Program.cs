d9f84418f0cca30c5079ac8841d76e152ccc86dc
sky <2336526392@qq.com>
2022-07-09 17:37:15 +0800

    MultiThreading Homework

--- hw1/hw1/Program.cs
+++ hw1/hw1/Program.cs
@@ -36,21 +36,24 @@ namespace Homework1
         public void Produce()
         {
             // 可以加东西
-            
+            P(sem);
             Producer.ProduceACake(cake); // 这句话不允许改，但可以在前后加代码
-            
+            V(sem);
             // 可以加东西
         }
 
         public void Consume()
         {
             // 可以加东西
-            
-            Consumer.ConsumeACake(cake); // 这句话不允许改，但可以在前后加代码
-
+            P(sem);
+            if (cake.Num >= 1)
+            {
+                Consumer.ConsumeACake(cake); // 这句话不允许改，但可以在前后加代码
+            }
+            V(sem);
             // 可以加东西
         }
-        
+        Semaphore sem = new Semaphore(1, 1);
         // 可以加东西
 
 // ==================================================================================================================  
